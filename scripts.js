38fcc9552212776bc3d88a63c76a86100854ecd3
Hoshigaru <hoshigaru7@gmail.com>
2023-11-26 15:31:20 -0500

    Add full functionality and CSS styles
    Everything works for the most part now. There are still a few strange
    bugs to sort out, like what happens when a user chooses one operator and
    then another.
    
    The main thing I might still need to add is the ability to use negative
    numbers as the second number... right now the user cannot do that.
    
    I think if I change how my code works, it might be better. Instead of
    showing the operator in the display text and grabbing it from there, for
    example, it might be better to have the operators switch the calculator
    into a certain mode (like adding mode, etc.).

--- scripts.js
+++ scripts.js
@@ -18,42 +18,102 @@ let firstNum;
 let secondNum;
 let operator;
 
+let isResult = false;
+
 function operate(firstNum, operator, secondNum) {
+    let result;
+
+    if (operator == '/' && secondNum == 0) {
+        display.textContent = "Nice try!"
+        return;
+    }
+    
     switch (true) {
         case operator == '+':
-            operator = 'add';
+            result = add(firstNum, secondNum);
             break;
         case operator == '-':
-            operator = 'subtract';
+            result = subtract(firstNum, secondNum);
             break;
         case operator == '*':
-            operator = 'multiply';
+            result = multiply(firstNum, secondNum);
             break;
         case operator == '/':
-            operator = 'divide';
+            result = divide(firstNum, secondNum);
             break;
     }
 
-    display.textContent = operator(firstNum, secondNum);
+    if (result.toString().length >= 5) {
+        result = Math.round(result * 10000) / 10000;
+    }
+    display.textContent = result;
+}
+
+const operatorButtons = document.querySelectorAll('.operator');
+
+for (let i = 0; i < operatorButtons.length; i++) {
+    operatorButtons[i].addEventListener('click', () => {
+        if (isOperatingAlready()) calculate();
+    });
+}
+
+function isOperatingAlready() {
+    let operationArray = display.textContent.split(/([-+/*])/);
+
+    if (operationArray[0] == '' && operationArray[1] == '-') {
+        operationArray.splice(0, 2);
+    }
+
+    if (operationArray.includes('+') || operationArray.includes('-') || operationArray.includes('*') || operationArray.includes('/')) {
+        return true;
+    }
 }
 
 const buttons = document.querySelectorAll('.button');
-
-let displayVariables = [];
 const display = document.querySelector('#display');
 
-for (let i = 0; i < buttons.length - 1; i++) {
-    buttons[i].addEventListener('click', () => display.textContent += buttons[i].textContent);
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', () => {
+        if (isDivideByZeroMsg() || (isZero() && !(buttons[i].classList.contains('operator'))) || (isResult && !(buttons[i].classList.contains('operator')))) {
+            display.textContent = '';
+            isResult = false;
+        }
+        display.textContent += buttons[i].textContent;
+        isResult = false;
+    });
+}
+function isZero() {
+    return display.textContent == '0';
+}
+
+function isDivideByZeroMsg () {
+    return display.textContent == 'Nice try!';
 }
 
 const clearButton = document.querySelector('#clear');
-clearButton.addEventListener('click', () => display.textContent = '');
+clearButton.addEventListener('click', () => {
+    display.textContent = '';
+});
+
+function calculate() {    
+    let displayArray = display.textContent.split(/([-+/*])/);
+
+    if (displayArray[0] == '' && displayArray[1] == '-')
+    {
+        displayArray.splice(0, 2);
+        firstNum = Number('-' + displayArray[0])
+    }
+    else {
+        firstNum = Number(displayArray[0]);
+    }
+    operator = displayArray[1];
+    secondNum = Number(displayArray[2]);
+
+    operate(firstNum, operator, secondNum);
+    isResult = true;
+}
 
 const equalButton = document.querySelector('#equal');
-
-displayVariables = display.textContent.split(/\+\-\/\*/);
-firstNum = displayVariables[0];
-operator = display.textContent.substring(firstNum.length, firstNum.length + 1);
-secondNum = displayVariables[1];
-
-equalButton.addEventListener('click', operate(firstNum, operator, secondNum))
+equalButton.addEventListener('click', () => {
+    if (isOperatingAlready()) calculate();
+});
